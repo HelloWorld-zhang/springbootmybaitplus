bb04c5b6094d4920c1e2ba2d77e392815ae1b8c9
zhanghe <2523637863@qq.com>
2021-05-24 10:36:02 +0800

    mybatis-plus

--- src/test/java/com/example/test/SampleTest.java
+++ src/test/java/com/example/test/SampleTest.java
@@ -1,18 +1,20 @@
 package com.example.test;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.entity.User;
 import com.example.mapper.UserMapper;
 import com.example.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 @SpringBootTest
 public class SampleTest {
@@ -70,18 +72,19 @@ public class SampleTest {
         int i = userMapper.updateById(user);
         System.out.println("影响的行数："+ i);
     }
-
+    //根据id删除数据
     @Test
     public void delete(){
         int i = userMapper.deleteById("1395570306390736897");
         System.out.println("受影响的行数:"+ i);
     }
+    //统计出总数
     @Test
     public void testCount(){
         int count = userService.count();
         System.out.println("总记录数："+ count);
     }
-
+    //批量添加数据
     @Test
     public void testSaveBatch(){
         ArrayList<User> users = new ArrayList<>();
@@ -93,12 +96,13 @@ public class SampleTest {
         }
         userService.saveBatch(users);
     }
-
+    //根据姓名查询
     @Test
     public void testSelectAllByName(){
         List<User> users = userService.selectAllByName("Jone");
         users.forEach(System.out::println);
     }
+    //分页
     @Test
     public void testPage(){
         Page<User> userPage = new Page<>(1, 3);
@@ -113,5 +117,101 @@ public class SampleTest {
         List<User> records = userPage.getRecords();
         records.forEach(System.out::println);
     }
+    //查询名字中包含n 年龄大于等于10且小于20的 邮箱不能为空
+    @Test
+    public void test1(){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("name","n")
+                    .between("age",10,30)
+                    .isNotNull("email");
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
+    }
+    //按照年龄降序 如果年龄相等按照id升序
+    @Test
+    public void test2(){
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        userQueryWrapper.orderByDesc("age").orderByAsc("id");
+        List<User> users = userMapper.selectList(userQueryWrapper);
+        users.forEach(System.out::println);
+    }
+    //删除邮箱为空的数据
+    @Test
+    public void  test3(){
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        userQueryWrapper.isNull("email");
+        int delete = userMapper.delete(userQueryWrapper);
+        System.out.println( delete);
+    }
+    //根据名字包含n的和年龄小于18岁并邮箱为空
+    @Test
+    public void test4(){
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.like("name","n").and(i-> i.lt("age",18).or().isNull("email"));
+        User user = new User();
+        user.setAge(18);
+        user.setEmail("user@qq.com");
+        int update = userMapper.update(user, wrapper);
+        System.out.println(update);
+    }
+    //根据名字和年龄查询
+    @Test
+    public void test5(){
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.select("name","age");
+        List<Map<String, Object>> maps = userMapper.selectMaps(wrapper);
+        maps.forEach(System.out::println);
+    }
+    @Test
+    public void test6(){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        //queryWrapper.inSql("id","select * from user wher id <= 3 or true");
+        queryWrapper.le("id",3);
+        List<Map<String, Object>> maps = userMapper.selectMaps(queryWrapper);
+        maps.forEach(System.out::println);
+    }
+    @Test
+    public void test7(){
+        UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.set("age",18).set("email","user@qq.com").like("name","n").and(i->i.lt("age",18).or().isNull("email"));
+        User user = new User();
+        int update = userMapper.update(user, updateWrapper);
+        System.out.println(update);
+    }
 
+    @Test
+    public void test8(){
+        String name = null;
+        Integer ageBegin = 10;
+        Integer ageEnd = 20;
+
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.like(StringUtils.isNotBlank(name),"name","n").ge(ageBegin!=null,"age",ageBegin).le(ageEnd !=null,"age",ageEnd);
+        List<User> users = userMapper.selectList(wrapper);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void test9(){
+        String name = null;
+        Integer ageBegin = 10;
+        Integer ageEnd = 20;
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.like(StringUtils.isNotBlank(name),User::getName,"n").ge(ageBegin !=null,User::getAge,ageBegin).le(ageEnd!=null,User::getAge,ageEnd);
+        List<User> users = userMapper.selectList(wrapper);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void test10(){
+        LambdaUpdateWrapper<User> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper
+                .set(User::getAge,18)
+                .set(User::getEmail,"user@zhang.com")
+                .like(User::getName,"n")
+                .and(i->i.lt(User::getAge,18).or().isNotNull(User::getEmail));
+        User user = new User();
+        int update = userMapper.update(user, updateWrapper);
+        System.out.println(update);
+    }
 }
